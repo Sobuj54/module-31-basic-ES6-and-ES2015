102f11d2a005b3edf2c8699011b9f8fb938e38f5
sobuj <sobujahmed124@gmail.com>
2023-02-21 20:45:45 +0600

    more arrow function

--- js/more-arrow.js
+++ js/more-arrow.js
@@ -13,17 +13,17 @@ const getPI = () => 3.14;
 
 // ------
 // one parameter arrow function
-const doubleIt = (a) => a*2;
+const doubleIt = (a) => a * 2;
 // simple way of one parameter arrow function
-const fiveTimes = a => a*5 ;
+const fiveTimes = (a) => a * 5;
 
 // -------
 // multiline arrow function
 // in multi line arrow function we need to use return inside curly brace
-const doMath = (x.y,z) => {
-    const firstAdd = x + y;
-    const secondAdd = y+z;
-    const multiply = firstAdd * secondAdd ;
-    const result = multiply / 2 ;
-    return result;
-}
+const doMath = (x, y, z) => {
+  const firstAdd = x + y;
+  const secondAdd = y + z;
+  const multiply = firstAdd * secondAdd;
+  const result = multiply / 2;
+  return result;
+};
